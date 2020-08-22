e3db3d78647112646d64466a841c6588676ad454
Miguel Macias <miguelfernandomacias@gmail.com>
2020-08-22 14:33:42 -0400

    resultado

--- emprendedor1.rb
+++ emprendedor1.rb
@@ -4,3 +4,10 @@ gastos = ARGV[2].to_i
 
 utilidad = precio * usuarios - gastos
 puts "la utilidad es de #{utilidad}"
+
+if utilidad > 0
+  impuestos = 0.65 * utilidad
+  puts "la utilidad despues de impuestos es   #{impuestos}"
+else 'la utilidad fue negativa no corresponde impuestos'
+
+end
